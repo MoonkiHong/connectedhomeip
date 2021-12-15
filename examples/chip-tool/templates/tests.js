0cf9df5cad8b180f9e066e877b5021f053d1e855
Morozov-5F <evgeniy.morozov@dsr-corporation.com>
2021-12-15 21:27:51 +0300

    Add YAML tests for Door Lock cluster (users/credentials)

--- examples/chip-tool/templates/tests.js
+++ examples/chip-tool/templates/tests.js
@@ -214,6 +214,7 @@ function getTests()
     'TestOperationalCredentialsCluster',
     'TestModeSelectCluster',
     'TestGroupMessaging',
+    'TestDoorLockCluster',
   ];
 
   const SoftwareDiagnostics = [
